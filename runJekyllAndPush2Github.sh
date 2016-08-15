05794e479d5b9748eb639fed87f9f443bc5b1d84
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-15 12:33:10 +0530

    Adding script on Mon Aug 15 12:33:10 IST 2016

---
+++ runJekyllAndPush2Github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Running Jekyll & Pushing to Github"
+echo "========================"
+
+message=${1:-"Adding script on `date`"}  
+echo $message
+cd "/media/deadcoder0904/TIMEPASS/Coding/Awesome Templates/Personal Site/best-a2k/blog"
+bundle exec jekyll build
+cp README.md ../blog-pages
+cd "../blog-pages"
+push2Github
+
+echo
+echo "========================"
+echo "Jekyll Content Served & Github Push Successful"
