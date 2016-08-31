7cbd325a563e6f643ac4b2053b35a323abbdae3d
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-31 11:34:05 +0530

    Adding script on Wed Aug 31 11:34:05 IST 2016

---
+++ runJekyllAndServe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Serving Jekyll"
+echo "========================"
+
+cd "/media/deadcoder0904/TIMEPASS/Coding/Awesome Templates/Personal Site/best-a2k/blog"
+bundle exec jekyll serve
+
+echo
+echo "========================"
+echo "Jekyll Content Served at localhost:4000"
