07b80f39bef2dad0afea45b96a19602dde7c4814
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-01 17:55:19 +0530

    Adding script on Tue Nov  1 17:55:19 IST 2016 :blush:

--- runHexoAndPush2Github.sh
+++ runHexoAndPush2Github.sh
@@ -2,15 +2,20 @@
 echo "Running Hexo & Pushing to Github"
 echo "========================"
 
+cd "/media/deadcoder0904/DEAD/Coding/General/blog"
 push2Github # Push Hexo Code to another Github repo
 
-cd "/media/deadcoder0904/DEAD/Coding/General/blog"
 hexo generate # Generate static files
 
 cd public	# Static HTML directory
 
 push2Github # Push Hexo Sites Code to another Github repo
-checkoutMasterAndMergeWithGhpages # Deploy the Page on Github-Pages
+
+# Deploy the Page on Github-Pages
+
+git checkout gh-pages
+git merge master
+git push origin gh-pages
 
 echo
 echo "========================"
