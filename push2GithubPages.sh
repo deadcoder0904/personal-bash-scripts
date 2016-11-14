3b2761ac1160563c6d05c1e73c0b9d742cdc00be
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-14 16:40:04 +0530

    Adding script on Mon Nov 14 16:40:04 IST 2016 :octocat:

---
+++ push2GithubPages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Checkout gh-pages And merge with master"
+echo "####################################################"
+
+push2Github
+checkoutGhpagesAndMergeWithMaster
+
+echo
+echo "####################################################"
+echo "Checkout gh-pages And merge with master Successful"
