27c19c157c6c865ebcb3776c790d43fde59ff794
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-16 12:37:52 +0530

    Adding script on Wed Nov 16 12:37:52 IST 2016 :octocat:

---
+++ makeHtmlTemplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Node JS Start Template"
+echo "##############################################################################################################"
+echo
+
+cp -av "/media/deadcoder0904/TIMEPASS/Coding/Template Scripts/HTML5,CSS3,Jquery,Tachyons" $1
+
+echo
+echo "##############################################################################################################"
+echo "Node JS End Template"
