f8de0482f748af5e848ed00fc669bca8572e410f
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-22 20:50:56 +0530

    Adding script on Tue Nov 22 20:50:56 IST 2016 :skin-tone-5:

--- sortAndOrderFiles.py
+++ sortAndOrderFiles.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 import errno
-from pprint import pprint
+
 search_dir = os.getcwd()
 
 os.chdir(search_dir)
 files = filter(os.path.isfile, os.listdir(search_dir))
-files = [os.path.join(search_dir, f) for f in files] # add path to each file
+files = [os.path.join(search_dir, f) for f in files] 
 files.sort(key=lambda x: os.path.getmtime(x))
 
 dir = "sorted"
