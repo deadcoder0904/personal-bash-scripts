45f2c8dd89e4da0a1a62b21afea409734eb0d71e
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-22 20:14:43 +0530

    Adding script on Tue Nov 22 20:14:43 IST 2016 :no_pedestrians:

---
+++ runGithubTrending.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Run Github Trending"
+echo "####################################################"
+
+cd "/media/deadcoder0904/DEAD/Coding/100dayz/github-trending-json-api"
+yarn start
+push2Github
+
+echo
+echo "####################################################"
+echo "Run Github Trending Successful"
---
+++ sortAndOrderFiles.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import errno
+from pprint import pprint
+search_dir = os.getcwd()
+
+os.chdir(search_dir)
+files = filter(os.path.isfile, os.listdir(search_dir))
+files = [os.path.join(search_dir, f) for f in files] # add path to each file
+files.sort(key=lambda x: os.path.getmtime(x))
+
+dir = "sorted"
+
+os.system("sudo mkdir -p " + dir)
+
+i=1
+for filename in files:
+	destination = "sorted/" + str(i) + ".jpg"
+	shutil.copy(filename,destination)
+	i = i + 1
+print "Copied all files to 'sorted' directory"
