9c694b027f8bf54636fd4ca9292d58d1bab5fd0d
Akshay Kadam <akshaykadam0904@gmail.com>
2016-12-11 20:08:42 +0530

    Adding script on Sun Dec 11 20:08:42 IST 2016 :oncoming_bus:

---
+++ makeHTMLCSSJSES7Template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Advanced HTML Start Template"
+echo "##############################################################################################################"
+echo
+
+cp -av "/media/deadcoder0904/DEAD/Coding/General/template-scripts/HTML5-CSS3-JS-ES7" $1
+
+echo
+echo "##############################################################################################################"
+echo "Advanced HTML End Template"
