c9ecb56717252850721e4c52a11cb4a993786e70
Akshay Kadam <akshaykadam0904@gmail.com>
2017-01-13 16:42:07 +0530

    Adding script on Fri Jan 13 16:42:07 IST 2017 :small_red_triangle_down:

---
+++ makeSimpleReactTemplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "HTML Start Template"
+echo "##############################################################################################################"
+echo
+
+cp -av "/media/deadcoder0904/DEAD/Coding/General/template-scripts/SimpleReact" $1
+
+echo
+echo "##############################################################################################################"
+echo "HTML End Template"
