f4992644d234aa9908d19079e5838467666b357b
Akshay Kadam <akshaykadam0904@gmail.com>
2017-04-30 21:33:06 +0530

    Added C++ 11 support :fire:

--- runC++.sh
+++ runC++.sh
@@ -2,7 +2,7 @@
 echo "Running C++ Program $1"
 echo "========================"
 
-g++ $1 # Compile C Program
+g++ -std=c++11 $1 # Compile C Program
 ./a.out # Display the output of C++ Program
 rm -rf a.out # Remove the output of C++ Program
 
