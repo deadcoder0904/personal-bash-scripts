4fa1b7fa9adea2db88bfebe7e9e63387a2ad72d4
Akshay Kadam <akshaykadam0904@gmail.com>
2017-06-03 15:31:46 +0530

    Run Emulator :wink:

---
+++ runEmulator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Running Emulator"
+echo "========================"
+
+cd ~/Android/Sdk/tools
+./emulator -avd Nexus_5X_API_24
+
+echo
+echo "========================"
+echo "Emulator Successfully Ran"
