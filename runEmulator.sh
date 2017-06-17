fbc91442c8710f79edacb16b435ae40f0d05e4f9
Akshay Kadam <akshaykadam0904@gmail.com>
2017-06-17 13:41:13 +0530

    Update Emulator Script :heart_eyes:

--- runEmulator.sh
+++ runEmulator.sh
@@ -2,7 +2,7 @@
 echo "Running Emulator"
 echo "========================"
 
-cd ~/Android/Sdk/tools
+cd /media/deadcoder0904/Akshay/Akshay/Android/Sdk/tools
 ./emulator -avd Nexus_5X_API_24 -gpu off
 
 echo
